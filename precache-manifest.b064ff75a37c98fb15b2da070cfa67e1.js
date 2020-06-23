eccb24473bac2cd5f7f1a40bac91542dcc928ae8
lakshmisri <vijayasri1811@gmail.com>
2020-06-23 22:57:37 +0530

    Updates

---
+++ precache-manifest.b064ff75a37c98fb15b2da070cfa67e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37cdc08817c1888c76a585988607ed7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a67ba14c62b1a4391f69",
+    "url": "/react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a985db1741ab996abb6",
+    "url": "/react-gh-pages/static/js/2.84e09d9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.84e09d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67ba14c62b1a4391f69",
+    "url": "/react-gh-pages/static/js/main.03a7dfff.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  }
+]);
